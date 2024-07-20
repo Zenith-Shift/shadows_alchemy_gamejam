e8115e17782f1ab81d18d1b5dd54522092ec1564
Khalid <zkac429@live.rhul.ac.uk>
2024-07-20 13:54:30 +0100

    Changed the camera switch and added a barrier

--- CameraChange.gd
+++ CameraChange.gd
@@ -3,10 +3,13 @@ extends Area3D
 var entered = false
 
 func _on_body_entered(body: PhysicsBody3D):
-	entered = not entered
+	if body.name == "Player":
+		entered = true
 
-func _process(delta):
-	if entered:
-		$"../Camera3D".position.x = 3.0
-	else:
-		$"../Camera3D".position.x = -13.186
+func _on_body_exited(body: PhysicsBody3D):
+	if body.name == "Player" and entered:
+		call_deferred("_disable_collision_and_move_camera")
+
+func _disable_collision_and_move_camera():
+	$"../Barrier/CollisionShape3D".disabled = false
+	$"../Camera3D".position.x = 3.0
